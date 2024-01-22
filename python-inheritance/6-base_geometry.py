ecfe02a3dbaf0f8fda62a0bb1c981e68b5ca262b
EJMarkle <emarkle99@gmail.com>
2024-01-22 17:09:30 -0600

    6-base_geometry

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Creates an empty class called BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """empty class called BaseGeometry"""
+    def area(self):
+        raise Exception("area() is not implemented")
