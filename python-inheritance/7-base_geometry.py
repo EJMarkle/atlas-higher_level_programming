01eda9b4ca07e44678342e22bee9e6ee4af1a975
EJMarkle <emarkle99@gmail.com>
2024-01-22 17:40:06 -0600

    int validator fix + test typos

--- python-inheritance/7-base_geometry.py
+++ python-inheritance/7-base_geometry.py
@@ -11,6 +11,6 @@ class BaseGeometry:
     def integer_validator(self, name, value):
         """Checks if integer and if int over 0"""
         if type(value) is not int:
-            raise TypeError("<name> must be an integer")
+            raise TypeError("{} must be an integer".format(name))
         if value <= 0:
-            raise ValueError("<name> must be greater than 0")
+            raise ValueError("{} must be greater than 0".format(name))
