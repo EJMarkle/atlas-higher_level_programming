87de81505ba9eed4266b9e0a0e1c78c4eb2c8acd
EJMarkle <emarkle99@gmail.com>
2024-01-23 16:41:23 -0600

    4-fromjsontostring

---
+++ python-input_output/4-from_json_string.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""A function that returns a python object represented by a JSON string."""
+import json
+
+
+def from_json_string(my_str):
+    """returns obj from json"""
+    return.loads(my_str)
