9dca96b5d36abd17099d42d0390bc65b7e2c365b
EJMarkle <emarkle99@gmail.com>
2024-01-23 16:57:21 -0600

    6-loadfromjson

---
+++ python-input_output/6-load_from_json_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/pyton3
+"""A function that creates an object from a JSON file."""
+import json
+
+
+def load_from_json_file(filename):
+    """creates obj from json"""
+    with open(filename, "r", encoding="utf-8") as file:
+        return json.load(file)
