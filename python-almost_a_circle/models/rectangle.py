02f30978d43e6f2e931aa0b3ca2e2400bba187d7
EJMarkle <emarkle99@gmail.com>
2024-01-28 16:33:45 -0600

    pep8

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """Creates the class Rectangle"""
-
-
 from models.base import Base
+
+
 class Rectangle(Base):
     """ Sets width, height, x, and y of rectangle """
     def __init__(self, width, height, x=0, y=0, id=None):
-       super().__init__(id) 
-       self.__width = width
-       self.__height = height
-       self.__x = x
-       self.__y = y
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
     @property
     def width(self):
         """ Width getter"""
         return self.__width
-    
+
     @width.setter
     def width(self, value):
         """ Width setter"""
@@ -30,7 +30,7 @@ class Rectangle(Base):
     def height(self):
         """Height getter"""
         return self.__height
-    
+
     @height.setter
     def height(self, value):
         """Height setter"""
@@ -44,7 +44,7 @@ class Rectangle(Base):
     def x(self):
         """X getter"""
         return self.__x
-    
+
     @x.setter
     def x(self, value):
         """X setter"""
@@ -56,7 +56,7 @@ class Rectangle(Base):
     def y(self):
         """Y getter"""
         return self.__y
-    
+
     @y.setter
     def y(self, value):
         """ Y setter"""
