0236c84c0a4c1c04d374e0470a25247982ce13ca
EJMarkle <emarkle99@gmail.com>
2024-01-29 17:32:30 -0600

    syntax fix + wrong attr fix

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -6,8 +6,7 @@ from models.rectangle import Rectangle
 class Square(Rectangle):
     """The class 'Square', which inherits from 'Rectangle'"""
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(self, size, x, y. id)
+        super().__init__(size, size, x, y, id)
 
     def __str__(self):
         return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.width)
-    
