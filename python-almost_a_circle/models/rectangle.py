d574d535ec811694d839c82a4f5f0f1a2eb77f14
EJMarkle <emarkle99@gmail.com>
2024-01-28 18:01:59 -0600

    pep8 silvia

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -28,7 +28,8 @@ class Rectangle(Base):
 
     def __str__(self):
         """Returns string representation of Rectangle"""
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.x, self.y, self.width, self.height)
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                    self.x, self.y, self.width, self.height)
 
     @property
     def width(self):
