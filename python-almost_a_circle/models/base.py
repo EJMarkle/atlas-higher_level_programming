527360654273f4c82f847bcc06f3a3fa9db2faa7
EJMarkle <emarkle99@gmail.com>
2024-01-29 18:35:03 -0600

    ok really list fix

--- python-almost_a_circle/models/base.py
+++ python-almost_a_circle/models/base.py
@@ -26,7 +26,7 @@ class Base:
     def from_json_string(json_string):
         """Returns list of the JSON representation json_string"""
         if json_string is None or len(json_string) == 0:
-            return "[ ]"
+            return []
         else:
             return json.loads(json_string)
 
