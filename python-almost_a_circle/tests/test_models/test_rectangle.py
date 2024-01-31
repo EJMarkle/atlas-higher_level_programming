6cc0c06414f97d6b33437e6f7aab3d8bfe7c13c9
EJMarkle <emarkle99@gmail.com>
2024-01-30 20:40:11 -0600

    more rectangle test

--- python-almost_a_circle/tests/test_models/test_rectangle.py
+++ python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -7,6 +7,63 @@ from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
+    def test_init(self):
+        Base._Base__nb_objects = 0
+        
+        r1 = Rectangle(1, 2)
+        self.assertEqual(r1.width, 1)
+        self.assertEqual(r1.height, 2)
+        self.assertEqual(r1.x, 0)
+        self.assertEqual(r1.y, 0)
+        self.assertEqual(r1.id, 1)
+
+        r2 = Rectangle(1, 2, 3)
+        self.assertEqual(r2.width, 1)
+        self.assertEqual(r2.height, 2)
+        self.assertEqual(r2.x, 3)
+        self.assertEqual(r2.y, 0)
+        self.assertEqual(r2.id, 2)
+
+        r3 = Rectangle(1, 2, 3, 4)
+        self.assertEqual(r3.width, 1)
+        self.assertEqual(r3.height, 2)
+        self.assertEqual(r3.x, 3)
+        self.assertEqual(r3.y, 4)
+        self.assertEqual(r3.id, 3)
+
+        with self.assertRaises(TypeError):
+            Rectangle("1", 2)
+
+        with self.assertRaises(TypeError):
+            Rectangle(1, "2")
+
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, "3")
+
+        with self.assertRaises(TypeError):
+            Rectangle(1, 2, 3, "4")
+
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, 3, 4, 5)
+
+        with self.assertRaises(ValueError):
+            Rectangle(-1, 2)
+
+        with self.assertRaises(ValueError):
+            Rectangle(-1, -2)
+
+        with self.assertRaises(ValueError):
+            Rectangle(0, 2)
+
+        with self.assertRaises(ValueError):
+            Rectangle(1, 0)
+
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, -3)
+
+        with self.assertRaises(ValueError):
+            Rectangle(1, 2, 3, -4)
+
     def test_area(self):
         r = Rectangle(4, 5)
         self.assertEqual(r.area(), 20)
@@ -14,6 +71,18 @@ class TestRectangle(unittest.TestCase):
     def test_str(self):
         r = Rectangle(4, 5, 1, 2, 7)
         self.assertEqual(str(r), "[Rectangle] (7) 1/2 - 4/5")
+
+    def test_display_without_x_y(self):
+        r = Rectangle(2, 3)
+        r.display()
+
+    def test_display_without_y(self):
+        r = Rectangle(2, 3, 1)
+        r.display()
+
+    def test_display_exists(self):
+        r = Rectangle(2, 3, 1, 2)
+        r.display()
 
     def test_to_dictionary(self):
         r = Rectangle(4, 5, 1, 2, 7)
