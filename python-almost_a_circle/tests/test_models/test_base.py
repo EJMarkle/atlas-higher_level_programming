7a9744d405ccc12fde45bbdc3a03819a5521a0cf
EJMarkle <emarkle99@gmail.com>
2024-01-30 18:10:27 -0600

    base class some tests

---
+++ python-almost_a_circle/tests/test_models/test_base.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Unit tests for 'Base' class methods'"""
+import os
+import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    def test_to_json_string(self):
+        self.assertEqual(Base.to_json_string(None), "[]")
+        self.assertEqual(Base.to_json_string([]), "[]")
+        self.assertEqual(Base.to_json_string([{'id': 1, 'name': 'test'}]), '[{"id": 1, "name": "test"}]')
+
+    def test_from_json_string(self):
+        self.assertEqual(Base.from_json_string(None), [])
+        self.assertEqual(Base.from_json_string(''), [])
+        self.assertEqual(Base.from_json_string('[{"id": 1, "name": "test"}]'), [{'id': 1, 'name': 'test'}])
+
+    def tearDown(self):
+        try:
+            os.remove('Base.json')
+        except FileNotFoundError:
+            pass
+
+if __name__ == '__main__':
+    unittest.main()
