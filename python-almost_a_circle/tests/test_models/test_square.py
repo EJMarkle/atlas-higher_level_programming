e868abe39bedfc065d4498af70b04c6dda74bab4
EJMarkle <emarkle99@gmail.com>
2024-01-30 20:50:18 -0600

    pep8

--- python-almost_a_circle/tests/test_models/test_square.py
+++ python-almost_a_circle/tests/test_models/test_square.py
@@ -8,7 +8,6 @@ from models.square import Square
 
 class TestSquare(unittest.TestCase):
     def test_square_exists(self):
-        # Test cases for Square(1), Square(1, 2), Square(1, 2, 3), etc.
         square1 = Square(1)
         self.assertIsInstance(square1, Square)
 
@@ -18,31 +17,23 @@ class TestSquare(unittest.TestCase):
         square3 = Square(1, 2, 3)
         self.assertIsInstance(square3, Square)
 
-        # Add more test cases as needed
-
     def test_square_invalid_arguments(self):
-        # Test cases for invalid arguments like Square("1"), Square(1, "2"), etc.
         with self.assertRaises(TypeError):
             invalid_square1 = Square("1")
 
         with self.assertRaises(TypeError):
             invalid_square2 = Square(1, "2")
 
-        # Add more test cases as needed
-
     def test_str_method_exists(self):
-        # Test __str__() method for Square
         square = Square(1, 2, 3, 4)
         self.assertEqual(str(square), "[Square] (4) 2/3 - 1")
 
     def test_to_dictionary_method_exists(self):
-        # Test to_dictionary() method for Square
         square = Square(1, 2, 3, 4)
         expected_dict = {'id': 4, 'size': 1, 'x': 2, 'y': 3}
         self.assertEqual(square.to_dictionary(), expected_dict)
 
     def test_update_method_exists(self):
-        # Test update() method for Square
         square = Square(1, 2, 3, 4)
         square.update(5, 6, 7, 8)
         self.assertEqual(square.id, 5)
@@ -51,7 +42,6 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square.y, 8)
 
     def test_update_with_kwargs_exists(self):
-        # Test update() method with kwargs for Square
         square = Square(1, 2, 3, 4)
         square.update(id=5, size=6, x=7, y=8)
         self.assertEqual(square.id, 5)
@@ -60,7 +50,6 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(square.y, 8)
 
     def test_create_method_exists(self):
-        # Test create() method for Square
         square_dict = {'id': 5, 'size': 6, 'x': 7, 'y': 8}
         created_square = Square.create(**square_dict)
         self.assertIsInstance(created_square, Square)
@@ -70,13 +59,12 @@ class TestSquare(unittest.TestCase):
         self.assertEqual(created_square.y, 8)
 
     def test_save_to_file_method_exists(self):
-        # Test save_to_file() method for Square
+
         square = Square(1, 2, 3, 4)
         Square.save_to_file([square])
         self.assertTrue(os.path.isfile("Square.json"))
 
     def test_load_from_file_method_exists(self):
-        # Test load_from_file() method for Square
         square = Square(1, 2, 3, 4)
         Square.save_to_file([square])
         loaded_squares = Square.load_from_file()
