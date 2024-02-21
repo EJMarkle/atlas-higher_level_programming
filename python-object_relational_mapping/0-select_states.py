e59fe1fe7261e456eda2fda60567276b88d03f17
EJMarkle <emarkle99@gmail.com>
2024-02-20 22:26:51 -0600

    pycode line length

--- python-object_relational_mapping/0-select_states.py
+++ python-object_relational_mapping/0-select_states.py
@@ -11,7 +11,8 @@ if __name__ == "__main__":
         sys.exit(1)
     username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    db = MySQLdb.connect(host="localhost", port=3306, user=username, passwd=password, db=database)
+    db = MySQLdb.connect(host="localhost", port=3306, user=username,
+                         passwd=password, db=database)
     cursor = db.cursor()
 
     query = "SELECT * FROM states ORDER BY id ASC"
