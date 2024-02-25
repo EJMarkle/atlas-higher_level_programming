0eb205201737d814e31fd53ed63f8d83f8f595ea
unknown <emarkle99@gmail.com>
2024-02-25 13:25:26 -0600

    cmod fix?

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
-""" Prints exatly '"Programming is like building a multilingual puzzle' """
+""" Prints exactly '"Programming is like building a multilingual puzzle' """
 print('"Programming is like building a multilingual puzzle')
