7e55ca8b2dc2634af125e188bcdf7f7a0b58f2b4
EJMarkle <emarkle99@gmail.com>
2024-03-22 23:42:44 -0500

    argv in js

---
+++ javascript-warm_up/2-arguments.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const numAr = process.argv.length -2;
+if (numAr === 0) {
+    console.log("No argument");
+} else if (numAr === 1) {
+    console.log("Argument found");
+} else {
+    console.log("Arguments found");
+};
