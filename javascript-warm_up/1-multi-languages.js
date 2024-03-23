bf75a424a931b9a0c436cb902a0740b63b2df9f7
EJMarkle <emarkle99@gmail.com>
2024-03-22 23:07:01 -0500

    print 3 lines of fun

---
+++ javascript-warm_up/1-multi-languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const cFun = 'C is fun';
+console.log(cFun);
+const pythonCool = 'Python is cool';
+console.log(pythonCool);
+const jsAmazing = 'JavaScript is amazing';
+console.log(jsAmazing);
