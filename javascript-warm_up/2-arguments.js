a4ad9d624593a1abda6bb6c28e368c0ebf0ad7ba
EJMarkle <emarkle99@gmail.com>
2024-03-22 23:44:51 -0500

    semistandard fixes

--- javascript-warm_up/2-arguments.js
+++ javascript-warm_up/2-arguments.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const numAr = process.argv.length -2;
+const numAr = process.argv.length - 2;
 if (numAr === 0) {
-    console.log("No argument");
+  console.log('No argument');
 } else if (numAr === 1) {
-    console.log("Argument found");
+  console.log('Argument found');
 } else {
-    console.log("Arguments found");
-};
+  console.log('Arguments found');
+}
